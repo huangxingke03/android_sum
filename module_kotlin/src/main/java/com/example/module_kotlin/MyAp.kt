8332f8cd3d34be572bf67f39a935566424152eb1
huangxingke <huangxingke03@163.com>
2025-07-30 16:18:48 +0800

    remove : 删除多余代码

--- module_kotlin/src/main/java/com/example/module_kotlin/MyAp.kt
+++ module_kotlin/src/main/java/com/example/module_kotlin/MyAp.kt
@@ -6,10 +6,5 @@ import com.alibaba.android.arouter.launcher.ARouter
 class MyAp:Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            ARouter.openLog()
-            ARouter.openDebug()
-        }
-        ARouter.init(this)
     }
 }
