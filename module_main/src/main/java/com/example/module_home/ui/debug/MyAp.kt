18d70bb08bd4157abd42caa289392b70951a45d7
huangxingke <huangxingke03@163.com>
2025-07-30 16:07:00 +0800

    remove : 删除多余代码

--- module_main/src/main/java/com/example/module_home/ui/debug/MyAp.kt
+++ module_main/src/main/java/com/example/module_home/ui/debug/MyAp.kt
@@ -2,15 +2,9 @@ package com.example.module_home.ui.debug
 
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
-import com.example.module_utils.BuildConfig
 
 class MyAp:Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            ARouter.openLog()
-            ARouter.openDebug()
-        }
-        ARouter.init(this)
     }
 }
